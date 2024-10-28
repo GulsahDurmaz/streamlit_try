84a15088ec51a016627e3e749a5cecc117d23cc2
Gülsah Durmaz <zahidegulsahdurmaz@gmail.com>
2024-10-28 17:03:34 +0100

    a

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+# Configure the Streamlit page
+st.set_page_config(page_title="2020 US Presidential Election Dashboard",
+                #    layout='wide',
+                   page_icon=":bar_chart:",
+                   initial_sidebar_state="expanded")
+
 def main():
     st.title("My Streamlit App")
     st.write("Hello, world!")
